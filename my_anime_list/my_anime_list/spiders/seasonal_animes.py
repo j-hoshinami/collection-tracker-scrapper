ea84fcfbd3d77ac569f03fc757bbbd79a92ff5ee
j-hoshinami <jairohoshinami@gmail.com>
2026-03-17 01:46:23 -0300

    feat: add more itens
    added initial_date, end_date, available_on, genres, studios, premiered.

--- my_anime_list/my_anime_list/spiders/seasonal_animes.py
+++ my_anime_list/my_anime_list/spiders/seasonal_animes.py
@@ -1,4 +1,3 @@
-import time
 from collections.abc import AsyncIterator
 from typing import Any, AsyncIterator
 
@@ -17,18 +16,21 @@ class SeasonalAnimeSpider(scrapy.Spider):
     }
     
     async def start(self) -> AsyncIterator[Any]:
-        url = "https://myanimelist.net/anime/season"
+        url = "https://myanimelist.net/anime/season/2026/winter"
         
         yield scrapy.Request(
             url=url,
             headers=self.headers,
-            callback=self.parse_seasons,
+            # callback=self.parse_seasons,
+            callback=self.parse_anime_urls,
+            meta={'dont_merge_cookies': True},
         )
 
     def parse_seasons(self, response: Response):
         urls = response.xpath('//div[@class="horiznav_nav"]/ul/li/a[@href]/@href').getall()
 
-        sections_to_ignore = ["manga", "archive"]
+        sections_to_ignore = ["manga", "archive", "schedule", "later", "adapted", "...", "Fall 2025", "Spring 2026",
+                              "Summer 2026"]
 
         for url in urls:
             if any(section for section in sections_to_ignore if section in url):
@@ -37,7 +39,8 @@ class SeasonalAnimeSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=url,
                 headers=self.headers,
-                callback=self.parse_anime_urls
+                callback=self.parse_anime_urls,
+                meta={'dont_merge_cookies': True},
             )
 
         print("*" * 50)
@@ -47,12 +50,12 @@ class SeasonalAnimeSpider(scrapy.Spider):
     def parse_anime_urls(self, response: Response):
         urls = response.xpath('//h2[@class="h2_anime_title"]/a[@href]/@href').getall()
 
-        urls = ['https://myanimelist.net/anime/57658/Jujutsu_Kaisen__Shimetsu_Kaiyuu_-_Zenpen']
         for url in urls:
             yield scrapy.Request(
                 url=url,
                 headers=self.headers,
-                callback=self.parse_anime_details
+                callback=self.parse_anime_details,
+                meta={'dont_merge_cookies': True},
             )
 
     def parse_anime_details(self, response: Response):
@@ -62,21 +65,38 @@ class SeasonalAnimeSpider(scrapy.Spider):
         if score:
             score = float(score)
 
-        print('URL -> ', response.url)
-        print('SCORE -> ', score)
-        print(f"NAME: {name} - {score}")
-
-        episode_quant = response.xpath('//span[@class="dark_text" and contains(text(),"Episodes:")]/parent::div/text()').get()
+        episode_quant = response.xpath('//span[contains(text(),"Episodes:")]/following-sibling::text()').get()
         episode_quant = episode_quant.strip() if episode_quant else None
-        # initial_date = response.xpath()
-        # end_date = response.xpath()
-        # available_on = response.xpath()
-        # genres = response.xpath()
-        # studio = response.xpath()
-        # premiered = response.xpath()
+
+        initial_date = None
+        end_date = None
+        date = response.xpath('//span[contains(text(), "Aired:")]/following-sibling::text()').get()
+        if date:
+            clean_date = date.strip()
+            if " to " in clean_date:
+                parts = clean_date.split(" to ", 1)
+                initial_date = parts[0].strip()
+                end_date = parts[1].strip()
+            else:
+                initial_date = clean_date
+                end_date = None
+            
+        available_on = response.xpath('//a[contains(@class, "broadcast-item")]/@title').getall()
+
+        genres = response.xpath('//span[@itemprop="genre"]/text()').getall()
+
+        studios = response.xpath('//span[contains(text(), "Studios:")]/../a/text()').getall()
+
+        premiered = response.xpath('//span[contains(text(), "Premiered:")]/../a/text()').get()
 
         item = MyAnimeListItem()
         item["name"] = name
         item["score"] = score
         item["episode_quant"] = episode_quant
+        item["initial_date"] = initial_date
+        item["end_date"] = end_date
+        item["available_on"] = available_on
+        item["genres"] = genres
+        item["studios"] = studios
+        item["premiered"] = premiered
         yield item
